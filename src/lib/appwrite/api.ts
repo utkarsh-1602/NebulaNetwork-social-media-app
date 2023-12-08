2a08e6892f751d13825dc8ec3bb32beafde2e4c8
utkarsh-1602 <usking619@gmail.com>
2023-12-08 22:04:45 +0530

    In api.ts, we are creating a new user account and saving the user to Appwrite database

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
 import { ID } from "appwrite";
 import { INewUser } from "@/types";
-import { account } from "./config";
+import { account, appwriteConfig, avatars, databases } from "./config";
 
 export async function createUserAccount(user: INewUser) {
     try {
@@ -9,12 +9,52 @@ export async function createUserAccount(user: INewUser) {
             user.email,
             user.password,
             user.name
-        )
+        );
 
-        return newAccount;
+        if (!newAccount) throw Error;
+
+        // gets the avatar url 
+        const avatarUrl = avatars.getInitials(user.name);
+
+        const newUser = await saveUserToDB({
+            accountId: newAccount.$id,
+            email: newAccount.email,
+            name: newAccount.name,
+            imageUrl: avatarUrl,
+            username: user.username
+        })
+
+
+
+        return newUser;
 
     } catch (error) {
         console.log(error);
         return error;
+    }
+}
+
+// Saves the User to database 
+export async function saveUserToDB(user: {
+    accountId: string;
+    email: string;
+    name: string;
+    imageUrl: URL;
+    username?: string;
+
+}) {
+    // We will save a user (document) to appwrite database
+    try {
+        const newUser = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            ID.unique(),
+            user
+        )
+
+        return newUser;
+
+    } catch (error) {
+        console.log("Error: ", error)
     }
 }
