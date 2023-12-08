15c13d44aee5a89b516eb9de382c7653fdcab05c
utkarsh-1602 <usking619@gmail.com>
2023-12-08 15:51:30 +0530

    added: VITE_APPWRITE_PROJECT_ID in env

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,1 +1,8 @@
 // inside config.ts, I will be able to import all the appwrite functionalities
+import { Client, Account, Databases, Storage, Avatars } from "appwrite";
+
+export const appwriteConfig = {
+    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+
+
+}
