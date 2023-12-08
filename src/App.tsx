3bf807903ff2773e9f061429c843a4a01237ad0b
utkarsh-1602 <usking619@gmail.com>
2023-12-08 13:55:38 +0530

    refacrored: globals.css, tailwind.config.js

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div>App</div>
+        <h1 className="text-3xl font-bold underline">
+            Hello NebulaNetwork!
+        </h1>
     )
 }
 
