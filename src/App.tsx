58fe47fca45a50061a2be9fd172e0bd336e27096
utkarsh-1602 <usking619@gmail.com>
2023-12-08 14:16:14 +0530

    added: AuthLayout, RootLayout Route

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,25 @@ import './globals.css'
 import SignInForm from './_auth/forms/SignInForm'
 import SignUpForm from './_auth/forms/SignUpForm'
 import { Home } from './_root/pages'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
 
 const App = () => {
     return (
         <main className='flex h-screen'>
             <Routes>
                 {/* Public Route */}
-                <Route path='/sign-in' element={<SignInForm />} />
-                <Route path='/sign-up' element={<SignUpForm />} />
-
+                <Route element={<AuthLayout />}>
+                    <Route path='/sign-in' element={<SignInForm />} />
+                    <Route path='/sign-up' element={<SignUpForm />} />
+                </Route>
 
                 {/* Private Route */}
-                <Route index element={<Home />} />
-                {/* defining index: means it is a starting page */}
+                <Route element={<RootLayout />}>
+                    <Route index element={<Home />} />
+                    {/* defining index: means it is a starting page */}
+                </Route>
+
             </Routes>
         </main>
     )
