bb4470b0c480a209703cb3e3483aebddea5f4c90
utkarsh-1602 <usking619@gmail.com>
2023-12-09 23:36:12 +0530

    creating FileUploader Component using react-dropzone package

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { useCallback, useState } from "react";
+import { FileWithPath, useDropzone } from "react-dropzone";
+
 
 const FileUploader = () => {
+
+
+
+    const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+    }, [])
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
     return (
-        <div>FileUploader</div>
+        <div {...getRootProps()} className="flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer">
+            <input {...getInputProps()} className="cursor-pointer" />
+            {
+                isDragActive ?
+                    <p>Drop the files here ...</p> :
+                    <p>Drag 'n' drop some files here, or click to select files</p>
+            }
+        </div>
     )
 }
 
