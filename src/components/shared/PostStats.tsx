4a8d97f3f8703bb9a7401d858e2a1af630417b00
utkarsh-1602 <usking619@gmail.com>
2023-12-10 14:47:58 +0530

    added comment to remove missing useEffect Hook dependency

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -29,6 +29,7 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
 
     useEffect(() => {
         setIsSaved(!!savedPostRecord);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [currentUser]);
 
     const handleLikePost = (
