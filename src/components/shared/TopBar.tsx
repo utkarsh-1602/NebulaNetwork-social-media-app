5ad3a92583caf0315b02ef90b370d8397dcd17fb
utkarsh-1602 <usking619@gmail.com>
2023-12-11 01:07:36 +0530

    minor fixes

--- src/components/shared/TopBar.tsx
+++ src/components/shared/TopBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Button } from '../ui/button'
 import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations';
@@ -12,6 +12,7 @@ const TopBar = () => {
 
     useEffect(() => {
         if (isSuccess) navigate(0);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isSuccess]);
 
 
