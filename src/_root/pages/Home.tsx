e831d7b44d819e3197d12681fe9294ea145c9a87
utkarsh-1602 <usking619@gmail.com>
2023-12-10 13:18:32 +0530

    added multiFormatDateString and formatDateString in lib/utils.ts

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,4 +1,5 @@
 import Loader from "@/components/shared/Loader"
+import PostCard from "@/components/shared/PostCard";
 import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutations";
 import { Models } from "appwrite";
 
