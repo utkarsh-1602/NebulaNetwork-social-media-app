748ff4a4eabf028884a32efdc85f2cf259dc67ec
utkarsh-1602 <usking619@gmail.com>
2023-12-10 18:09:51 +0530

    searchbar created for searchPosts in Explore page

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { Input } from '@/components/ui/input'
+import React, { useState } from 'react'
 
 const Explore = () => {
+
+    const [searchValue, setSearchValue] = useState('')
+
     return (
-        <div>Explore</div>
+        <div className='explore-container'>
+            <div className='explore-inner_container'>
+                <h2 className='h3-bold md:h2-bold w-full'>Search Posts</h2>
+                <div className='flex gap-1 px-4 w-full rounded-lg bg-dark-4'>
+                    <img
+                        src="/assets/icons/search.svg"
+                        width={24}
+                        height={24}
+                        alt="search"
+                    />
+                    <Input
+                        type="text"
+                        placeholder="Search"
+                        className="explore-search"
+                        value={searchValue}
+                        onChange={(e) => setSearchValue(e.target.value)}
+
+                    />
+
+
+                </div>
+            </div>
+        </div>
     )
 }
 
