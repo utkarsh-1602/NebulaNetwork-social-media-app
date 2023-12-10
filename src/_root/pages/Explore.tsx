1010fa72b763d284751bfe7af06305a4dc504d23
utkarsh-1602 <usking619@gmail.com>
2023-12-10 18:12:15 +0530

    Popular div title, and all button created

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -7,6 +7,7 @@ const Explore = () => {
 
     return (
         <div className='explore-container'>
+
             <div className='explore-inner_container'>
                 <h2 className='h3-bold md:h2-bold w-full'>Search Posts</h2>
                 <div className='flex gap-1 px-4 w-full rounded-lg bg-dark-4'>
@@ -24,10 +25,24 @@ const Explore = () => {
                         onChange={(e) => setSearchValue(e.target.value)}
 
                     />
-
-
                 </div>
             </div>
+
+            <div className='flex-between w-full max-w-5xl mt-16 mb-7'>
+                <h3 className="body-bold md:h3-bold">Popular Today</h3>
+                <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
+                    <p className="small-medium md:base-medium text-light-2">All</p>
+                    <img
+                        src="/assets/icons/filter.svg"
+                        width={20}
+                        height={20}
+                        alt="filter"
+                    />
+                </div>
+            </div>
+
+
+
         </div>
     )
 }
